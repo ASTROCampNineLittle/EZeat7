1d76beda3028fb704cd6304456beaad01b6fea68
Seth <sepx898508@gmail.com>
2021-12-21 14:42:04 +0800

    Update 20211221044043_devise_create_companies.rb

--- db/migrate/20211221044043_devise_create_companies.rb
+++ db/migrate/20211221044043_devise_create_companies.rb
@@ -4,15 +4,15 @@ class DeviseCreateCompanies < ActiveRecord::Migration[7.0]
   def change
     create_table :companies do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      ## t.string :email,              null: false, default: ""
+      ## t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
+      ## t.string   :reset_password_token
+      ## t.datetime :reset_password_sent_at
 
       ## Rememberable
-      t.datetime :remember_created_at
+      ## t.datetime :remember_created_at
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false

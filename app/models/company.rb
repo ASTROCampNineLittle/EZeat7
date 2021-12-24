1042033a04da16d726866f915f2e5b0aaf49a5ea
Laichu <cozzichilli@gmail.com>
2021-12-24 23:16:43 +0800

    fix: some bugs in backend_company

--- app/models/company.rb
+++ app/models/company.rb
@@ -4,6 +4,5 @@ class Company < ApplicationRecord
   has_many :stores
 
   validates :name, :address, :tele,:manager_name, uniqueness: true, presence: true 
-  validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "請填入正確email格式"}
 
 end

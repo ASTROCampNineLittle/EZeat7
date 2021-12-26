766a2a564a6421c313d0b7959442dfba181f2a75
Laichu <cozzichilli@gmail.com>
2021-12-26 20:49:08 +0800

    fix: 將store表格的food_type資料類型改成integer並使用enum建立建立下拉選單

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_24_133047) do
+ActiveRecord::Schema.define(version: 2021_12_26_044211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 2021_12_24_133047) do
   create_table "stores", force: :cascade do |t|
     t.integer "company_id"
     t.string "name"
-    t.string "food_type"
+    t.integer "food_type"
     t.string "tel"
     t.string "email"
     t.text "intro"

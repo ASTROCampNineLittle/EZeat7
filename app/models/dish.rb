c4d4172e55552b7ac81999d6a995dae01a2d034a
ching <ching22h7@gmail.com>
2021-12-28 12:48:18 +0800

    feat: myorder_search

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,5 @@ class Dish < ApplicationRecord
 
   belongs_to :store
   has_many :open_date
+
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -2,5 +2,9 @@ class Store < ApplicationRecord
   has_many :dishes
   belongs_to :company
 
+  enum food_type: [:中式, :日式, :韓式, :印度, :義式, :英式, :美式, :法式, :德式, :墨西哥, :其它]
+
+  # enum food_type: [:chinese, :japanese, :korean, :indian, :italian, :british, :american, :french, :german, :mexican, :others]
+
   validates :name, :food_type, :tel,:email, presence: true 
 end

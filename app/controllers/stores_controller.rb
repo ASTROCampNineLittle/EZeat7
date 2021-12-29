7c6db052ba9fe47f67e075f7039d00cf8b64dffc
CHING <92965882+ching227@users.noreply.github.com>
2021-12-29 14:46:42 +0800

    feat: myorder頁面 全新的index頁面 (#44)
    * fix/conflict
    
    * feat: myorder_search
    
    * feat: new_homepage
    
    * feat: index頁面

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoresController < ApplicationController
 
   def index
-    @company = Company.find(2)
+    # @company = Company.find(2)
     # @store = @company.stores.find_by(:id [params:id])
   end
 
